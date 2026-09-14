2dad6c2e68e35e4632a98cfe6bfeade307aa4834
Syed Shuja Abbas <syedzaya123@gmail.com>
2026-09-14 20:32:20 +0530

    Add ConditionalStatements class for grade evaluation

---
+++ ConditionalStatements.java
@@ -0,0 +1,22 @@
+public class ConditionalStatements {
+    public static void main(String[] args) {
+
+        int marks = 75;
+
+        if (marks >= 90) {
+            System.out.println("Grade: A+");
+        } 
+        else if (marks >= 75) {
+            System.out.println("Grade: A");
+        } 
+        else if (marks >= 60) {
+            System.out.println("Grade: B");
+        } 
+        else if (marks >= 40) {
+            System.out.println("Grade: C");
+        } 
+        else {
+            System.out.println("Fail");
+        }
+    }
+}
