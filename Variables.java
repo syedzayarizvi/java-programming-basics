3231442e48c450b3c5006f201876a608db5c25bb
Syed Shuja Abbas <syedzaya123@gmail.com>
2026-09-14 20:31:00 +0530

    Create Variables.java

---
+++ Variables.java
@@ -0,0 +1,12 @@
+public class Variables {
+    public static void main(String[] args) {
+
+        String name = "Syed Shuja Abbas";
+        int age = 20;
+        double marks = 85.5;
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Marks: " + marks);
+    }
+}
